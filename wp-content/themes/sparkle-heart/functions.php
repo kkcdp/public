e6415c341ef170b67e0cf135830ceb785c709167
Kaspars <kaspars.bc@inbox.lv>
2023-10-28 23:09:51 +0300

    Oct 28, 2023

--- wp-content/themes/sparkle-heart/functions.php
+++ wp-content/themes/sparkle-heart/functions.php
@@ -6,7 +6,9 @@
 
 //Includes
 include(get_theme_file_path('/includes/front/enqueue.php')); 
+include(get_theme_file_path('/includes/front/head.php'));
 
 
 //Hooks
 add_action('wp_enqueue_scripts', 'sp_enqueue');
+add_action('wp_head', 'sp_head', 5);
--- wp-content/themes/sparkle-heart/includes/front/enqueue.php
+++ wp-content/themes/sparkle-heart/includes/front/enqueue.php
@@ -2,14 +2,22 @@
 
 function sp_enqueue(){
     wp_register_style(
+        'sp_font_montserrat_and_oswald',
+        'https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald:wght@500&display=swap',
+        [],
+        null
+    );
+
+    wp_register_style(
         'sp_normalize_css',
         'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
     );
     wp_register_style(
-        'sp_style_css',
+        'sp_theme',
         get_theme_file_uri('assets/css/style.css')
     );
 
+    wp_enqueue_style('sp_font_montserrat_and_oswald');
     wp_enqueue_style('sp_normalize_css');
-    wp_enqueue_style('sp_style_css');
+    wp_enqueue_style('sp_theme');
 }
